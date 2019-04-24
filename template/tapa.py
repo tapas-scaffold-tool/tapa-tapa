3779fe90a815911dc82bb376460719f0a965e6ae
Kirill Sulim <kirillsulim@gmail.com>
2019-04-24 16:45:52 +0500

    Add template

---
+++ template/tapa.py
@@ -0,0 +1,11 @@
+from tapas.tools import prompt
+
+
+# Require from user to enter parameters in this function
+def ask():
+    prompt('parameter_name')
+
+
+# Perform additional actions after generation in this function
+def post_init():
+    pass
